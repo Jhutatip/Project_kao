74b2c969532ba132156c57667352d7e0b8ae13af
dethMastery <me@suphakit.net>
2023-03-22 22:11:59 +0700

    fix: ticket path

--- index.ts
+++ index.ts
@@ -2,11 +2,15 @@ import express from "express";
 
 const app: express.Application = express();
 
-import {index} from "./src/page"
+import {index, ticket} from "./src/page"
 
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send(index());
 });
+
+app.get('/ticket', (req: express.Request, res: express.Response) => {
+  res.send(ticket());
+})
 
 app.listen(3000, () => {
   console.log("Server run on http://localhost:3000");
--- src/page.ts
+++ src/page.ts
@@ -9,3 +9,13 @@ export function index() {
   
   return page
 }
+
+export function ticket() {
+  let page = `<h1>
+  <center>
+    ChiengMai Zoo Ticket system
+  </center>
+</h1>`
+
+  return page
+}
