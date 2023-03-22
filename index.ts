e5006f259c5aab4b366bdb6af19f07c7ac4e2682
dethMastery <me@suphakit.net>
2023-03-22 21:58:28 +0700

    chore: refactoring

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
-import express, { Application, Request, Response } from 'express'
+import express from "express";
 
-const app: Application = express()
+const app: express.Application = express();
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
-})
+import {index} from "./src/page"
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send(index());
+});
 
 app.listen(3000, () => {
-    console.log('Server run on http://localhost:3000');
-})
+  console.log("Server run on http://localhost:3000");
+});
---
+++ src/page.ts
@@ -0,0 +1,11 @@
+export function index() {
+  let page = `<center>
+  <h1>
+    <center>
+      Hello, world!
+    </center>
+  </h1>
+</center>`
+  
+  return page
+}
